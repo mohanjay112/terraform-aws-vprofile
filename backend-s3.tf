8f603debb10f704f9ecc690579e8f8fdd3046400
mohanjay112 <mohanjay112@gmail.com>
2025-01-14 13:15:44 +0530

    Key-pairs

---
+++ backend-s3.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "terra-vprofile-state1122"
+    key = "terraform/backend"
+    region = "us-east-2"
+  }
+}
---
+++ keypairs.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.54.1"
+    }
+  }
+}
+resource "aws_key_pair" "vprofilekey" {
+  public_key = file(var.PUB_KEY_PATH)
+  key_name = "vprofilekey"
+}
