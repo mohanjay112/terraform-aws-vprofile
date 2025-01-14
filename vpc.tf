c192e98302b6902b1a2703ab5b2d28894442c3e8
mohanjay112 <mohanjay112@gmail.com>
2025-01-14 13:53:23 +0530

    sec grp

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = var.VPC_NAME
-  cidr = var.VpcCIDR
+  name            = var.VPC_NAME
+  cidr            = var.VpcCIDR
   azs             = [var.Zone1, var.Zone2, var.Zone3]
   private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
   public_subnets  = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
